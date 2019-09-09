61055d4f871272c67bc433b601ebd57278c416cc
githelsui <githelsuico@gmail.com>
2019-09-09 16:51:14 -0700

    update login

--- models.py
+++ models.py
@@ -8,7 +8,21 @@ class Tree(ndb.Model):
     email = ndb.StringProperty(required=True)
     date = ndb.StringProperty(required=True)
 
-# class Planter(ndb.Model):
-#     user_id = ndb.StringProperty(required=True)
-#     email = ndb.StringProperty(required=True)
-#     numberOfTrees = ndb.FloatProperty(required=True)
+class SentPostcard(ndb.Model):
+    # img = ndb.FloatProperty(required=True)
+    user_id = ndb.StringProperty(required=True)
+    message = ndb.StringProperty(required=False)
+    to = ndb.StringProperty(required=False)
+    date = ndb.StringProperty(required=True)
+
+class ReceivedPostcard(ndb.Model):
+    # img = ndb.FloatProperty(required=True)
+    user_id = ndb.StringProperty(required=True)
+    message = ndb.StringProperty(required=False)
+    to = ndb.StringProperty(required=False)
+    date = ndb.StringProperty(required=True)
+
+class User(ndb.Model):
+    user_id = ndb.StringProperty(required=True)
+    username = ndb.StringProperty(required=True)
+    numberOfTrees = ndb.FloatProperty(required=True)
